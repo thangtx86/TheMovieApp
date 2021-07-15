22cd557d7b3d62331a65965f8e36707b8f2b44b8
thang86 <tranthang8696@gmail.com>
2021-07-15 21:43:38 +0700

    change ui

--- lib/screens/signin/login_screen.dart
+++ lib/screens/signin/login_screen.dart
@@ -45,8 +45,217 @@ class _LoginWidgetState extends State<LoginWidget> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
         body: SingleChildScrollView(
+      child: Container(
+        child: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            Container(
+              height: size.height,
+              color: Color(0xFF243142),
+            ),
+            _buildHeadrLogin(),
+            _buildFormLogin(size)
+          ],
+        ),
+      ),
+    ));
+  }
+
+  Widget _buildFormLogin(Size size) {
+    return Positioned(
+      bottom: 0,
+      right: 0,
+      left: 0,
+      child: Container(
+        height: (size.height * 0.65),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(55),
+          ),
+        ),
+        child: Padding(
+          padding:
+              EdgeInsets.only(top: 60, left: 30.0, right: 30.0, bottom: 30.0),
+          child: Column(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(5),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                          color: Color.fromRGBO(143, 148, 251, .2),
+                          blurRadius: 20.0,
+                          offset: Offset(0, 10))
+                    ]),
+                child: _buildFormInput(),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              _buildButtonLogin(),
+              SizedBox(
+                height: 20,
+              ),
+              Text("Or", style: TextStyle(color: Colors.grey, fontSize: 22)),
+              SizedBox(
+                height: 20,
+              ),
+              _buildSocialNetworkButton(),
+              SizedBox(
+                height: 70,
+              ),
+              Text(
+                "Forgot Password?",
+                style: TextStyle(color: Color.fromRGBO(143, 148, 251, 1)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSocialNetworkButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        GestureDetector(
+          onTap: () {
+            _loginBloc.fbSignIn();
+          },
+          child: Container(
+            width: 145,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+            decoration: BoxDecoration(
+                color: Color(0xff0C90F2),
+                borderRadius: new BorderRadius.all(
+                  Radius.circular(20.0),
+                )),
+            child: Row(
+              children: <Widget>[
+                SvgPicture.asset(
+                  "assets/images/facebook.svg",
+                  width: 30,
+                  height: 30,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "Facebook",
+                  style: TextStyle(color: Colors.white, fontSize: 14),
+                )
+              ],
+            ),
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            _loginBloc.ggSignIn();
+          },
+          child: Container(
+            width: 145,
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 6),
+            decoration: BoxDecoration(
+                color: Color(0xffF55362),
+                borderRadius: new BorderRadius.all(
+                  Radius.circular(20.0),
+                )),
+            child: Row(
+              children: <Widget>[
+                SvgPicture.asset(
+                  "assets/images/google.svg",
+                  width: 30,
+                  height: 30,
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "Google",
+                  style: TextStyle(color: Colors.white, fontSize: 14),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildButtonLogin() {
+    return Container(
+      height: 50,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          gradient: LinearGradient(colors: [
+            Color.fromRGBO(143, 148, 251, 1),
+            Color.fromRGBO(143, 148, 251, .6),
+          ])),
+      child: Center(
+        child: Text(
+          "Login",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildFormInput() {
+    return Column(
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+              border: Border(bottom: BorderSide(color: Colors.grey[100]!))),
+          child: TextField(
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Email or Phone number",
+                hintStyle: TextStyle(color: Colors.grey[400])),
+          ),
+        ),
+        Container(
+          padding: EdgeInsets.all(8.0),
+          child: TextField(
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: "Password",
+                hintStyle: TextStyle(color: Colors.grey[400])),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget _buildHeadrLogin() {
+    return Positioned(
+      top: 100,
+      child: Align(
+        child: Container(
+          width: 80,
+          height: 80,
+          decoration: BoxDecoration(
+            color: Colors.green,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(16),
+              bottomLeft: Radius.circular(16),
+              bottomRight: Radius.circular(16),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginView() {
+    return SingleChildScrollView(
       child: Container(
         child: Column(
           children: <Widget>[
@@ -264,226 +473,6 @@ class _LoginWidgetState extends State<LoginWidget> {
           ],
         ),
       ),
-    ));
+    );
   }
 }
-
-/*class _LoginWidget extends StatelessWidget {
-  const _LoginWidget({Key? key}) : super(key: key);
-
-  LoginBloc _loginBloc;
-
-  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: Container(
-      child: Column(
-        children: <Widget>[
-          Container(
-            height: 400,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/images/background.png"),
-                  fit: BoxFit.fill),
-            ),
-            child: Stack(
-              children: <Widget>[
-                Positioned(
-                  width: 80,
-                  left: 30,
-                  height: 200,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/light-1.png"),
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  width: 80,
-                  left: 140,
-                  height: 150,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/light-2.png"),
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  top: 40,
-                  right: 40,
-                  width: 80,
-                  height: 150,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage("assets/images/clock.png"),
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  child: Container(
-                    margin: EdgeInsets.only(top: 50),
-                    child: Center(
-                      child: Text(
-                        "Login",
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 40,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(30.0),
-            child: Column(
-              children: <Widget>[
-                Container(
-                  padding: EdgeInsets.all(5),
-                  decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(10),
-                      boxShadow: [
-                        BoxShadow(
-                            color: Color.fromRGBO(143, 148, 251, .2),
-                            blurRadius: 20.0,
-                            offset: Offset(0, 10))
-                      ]),
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.all(8.0),
-                        decoration: BoxDecoration(
-                            border: Border(
-                                bottom: BorderSide(color: Colors.grey[100]!))),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Email or Phone number",
-                              hintStyle: TextStyle(color: Colors.grey[400])),
-                        ),
-                      ),
-                      Container(
-                        padding: EdgeInsets.all(8.0),
-                        child: TextField(
-                          decoration: InputDecoration(
-                              border: InputBorder.none,
-                              hintText: "Password",
-                              hintStyle: TextStyle(color: Colors.grey[400])),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Container(
-                  height: 50,
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      gradient: LinearGradient(colors: [
-                        Color.fromRGBO(143, 148, 251, 1),
-                        Color.fromRGBO(143, 148, 251, .6),
-                      ])),
-                  child: Center(
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text("Or", style: TextStyle(color: Colors.grey, fontSize: 22)),
-                SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        width: 145,
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 20, vertical: 6),
-                        decoration: BoxDecoration(
-                            color: Color(0xff0C90F2),
-                            borderRadius: new BorderRadius.all(
-                              Radius.circular(20.0),
-                            )),
-                        child: Row(
-                          children: <Widget>[
-                            SvgPicture.asset(
-                              "assets/images/facebook.svg",
-                              width: 30,
-                              height: 30,
-                            ),
-                            SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              "Facebook",
-                              style:
-                                  TextStyle(color: Colors.white, fontSize: 14),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      width: 145,
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 6),
-                      decoration: BoxDecoration(
-                          color: Color(0xffF55362),
-                          borderRadius: new BorderRadius.all(
-                            Radius.circular(20.0),
-                          )),
-                      child: Row(
-                        children: <Widget>[
-                          SvgPicture.asset(
-                            "assets/images/google.svg",
-                            width: 30,
-                            height: 30,
-                          ),
-                          SizedBox(
-                            width: 10,
-                          ),
-                          Text(
-                            "Google",
-                            style: TextStyle(color: Colors.white, fontSize: 14),
-                          )
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 70,
-                ),
-                Text(
-                  "Forgot Password?",
-                  style: TextStyle(color: Color.fromRGBO(143, 148, 251, 1)),
-                ),
-              ],
-            ),
-          )
-        ],
-      ),
-    ));
-  }
-}*/
